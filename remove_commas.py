6acf315e2af6766af15b5b32373b081f9b921995
Andrew Hundt <athundt@gmail.com>
2023-11-06 17:22:54 -0500

    remove_commas.py function works and renames correctly

--- remove_commas.py
+++ remove_commas.py
@@ -4,6 +4,8 @@ import json
 import pandas as pd
 import re
 import argparse
+import copy
+import csv
 
 
 def get_seed(filename):
@@ -28,6 +30,8 @@ name_dict = {}
 csv_list = []
 png_count = 0
 not_in_dict = 0
+files_in_csv_but_not_on_disk = 0
+png_files_missing_seed = 0
 png_set = set()
 csv_set = set()
 if os.path.exists(json_path):
@@ -42,11 +46,14 @@ for root, dirs, files in os.walk(folder):
             elif filename.endswith(".png"):
                 png_set.add(filename)
                 if "," in filename:
-                    new_name = filename.replace(",", "")
+                    new_name = filename.replace(",_", "_").replace(',','_').replace("__", "_")
                     name_dict[filename] = new_name
                     with open(json_path, "w") as f:
-                        json.dump(name_dict, f)
+                        json.dump(name_dict, f, indent=4)
+                # else:
+                #     name_dict[filename] = filename
 df_list = []
+step = 0
 for csv_path in csv_list:
     # Use quotechar to handle the double quotes in the CSV files
     df = pd.read_csv(csv_path, quotechar='"')
@@ -56,6 +63,7 @@ for csv_path in csv_list:
     for index, row in df.iterrows():
         seeds = []
         for col in row.index:
+            step += 1
             # Check if the column contains a PNG file
             if ".png" in row[col]:
                 dir, filename = os.path.split(row[col])
@@ -65,21 +73,31 @@ for csv_path in csv_list:
                     csv_set.add(filename)
                     png_count += 1
                     # Check if the filename is valid and exists in the folder
-                    if os.path.isfile(os.path.join(parent_dir, row[col])):
+                    file_to_check = os.path.join(folder, row[col])
+                    # print(f'is a png: {file_to_check} <<<<<<')
+                    if os.path.isfile(file_to_check):
+                        # print('filename exists on disk >>>>>>')
                         if filename in name_dict:
-                            filename = name_dict[filename]
-                            row[col] = os.path.join(dir, filename)
+                            # print(f'filename exists in dict: {filename}:{name_dict[filename]} >>>>>>')
+                            # print(f'{step} >>> row[col] before: {row[col]}')
+                            df.loc[index, col] = row[col].replace(filename, name_dict[filename])
+                            # print(f'{step} >>> row[col] after: {row[col]}')
                         else:
                             not_in_dict += 1
                         # Use filename instead of row[col] to get the seed
                         seed = get_seed(filename)
+                        print("filename: {filename} seed: {seed}")
                         # Check if the seed is not None before appending it
                         if seed is not None:
                             seeds.append(seed)
+                        else:
+                            print("Warning: The file {filename} was missing a generator seed.")
+                            png_files_missing_seed += 1
                     else:
                         # Print an error message
                         print(f"Error: {filename} is not a valid or existing file.")
-                    row[col] = os.path.join(parent_dir, row[col])
+                        files_in_csv_but_not_on_disk += 1
+                    # row[col] = os.path.join(parent_dir, row[col])
         # Use df.drop_duplicates to remove duplicate rows
         unique_seeds = pd.Series(seeds).drop_duplicates()
         if len(unique_seeds) == 1:
@@ -89,15 +107,28 @@ for csv_path in csv_list:
         else:
             # Print a warning with the csv file name, the row entry and the seeds
             print(f"Warning: The seeds in row {index} of {csv_path} are not consistent. The row entry is: {row}. The seeds are: {seeds}.")
-    df_list.append(df)
+    df_list.append(copy.deepcopy(df))
 percentage = round((not_in_dict / png_count) * 100, 2) if png_count > 0 else 0
-print(f"{percentage}% of {png_count} PNG files are not in the dictionary. This means that {not_in_dict} of the PNG files in the folder have not been renamed or do not exist.")
-# Use the set difference to show the PNG files that are not in the CSV files
-print(f"The following PNG files are not in the CSV files: {png_set - csv_set}. This means that some of the PNG files in the folder are not referenced in the CSV files.")
-print(f"There are {len(csv_list)} CSV files and {len(name_dict)} PNG files to be modified.")
 print("The following files will be renamed:")
 for old_name, new_name in name_dict.items():
     print(f"{old_name} -> {new_name}")
+set_diff = png_set - csv_set
+print("")
+print("Results of Dataset Cleanup and Summary Stats")
+print("--------------------------------------------")
+if len(set_diff) or files_in_csv_but_not_on_disk:
+    print(f"The following PNG files are not in the CSV files: {set_diff}. This means that some of the PNG files in the folder are not referenced in the CSV files.")
+    print(f"Error: The following is an error if greater than zero: {files_in_csv_but_not_on_disk} files are in a csv file but could not be found on the disk.")
+else:
+    print("Good: All PNG files in the CSV files were successfully found on disk.")
+
+if png_files_missing_seed:
+    print(f"Warning: {png_files_missing_seed} png files are missing a generator seed value.")
+else:
+    print("Good: All png files have a generator seed.")
+print(f"Files with commas: {not_in_dict} PNG files, or {percentage}% of the total {png_count} PNG files are not in the dictionary of files to rename. This means that their PNG files in the folder did not need to be renamed.")
+# Use the set difference to show the PNG files that are not in the CSV files
+print(f"There are {len(csv_list)} CSV files and {len(name_dict)} PNG files to be modified.")
 answer = input("Do you want to rename the files in the folder and save the csv files? (y/n) ")
 if answer.lower() == "y":
     for root, dirs, files in os.walk(folder):
@@ -108,7 +139,7 @@ if answer.lower() == "y":
                 os.rename(file_path, os.path.join(root, new_name))
                 print(f"Renamed {filename} to {new_name}.")
     for csv_path, df in zip(csv_list, df_list):
-        df.to_csv(csv_path, index=False)
+        df.to_csv(csv_path, index=False, quoting=csv.QUOTE_ALL)
         print(f"Saved {csv_path}.")
 else:
     print("No files were renamed or saved.")
