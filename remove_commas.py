e3c4f5c7b726a8ba55434dd345cc1b1c1fea2f30
Andrew Hundt <athundt@gmail.com>
2023-11-06 16:11:47 -0500

    remove_commas.py address some formatting issues

--- remove_commas.py
+++ remove_commas.py
@@ -5,20 +5,24 @@ import pandas as pd
 import re
 import argparse
 
+
 def get_seed(filename):
-    match = re.search(r"\d+", filename)
+    # Match the last group of digits before the .png extension
+    match = re.search(r"(\d+)\.png$", filename)
     if match:
-        return int(match.group())
+        return int(match.group(1))
     return None
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("folder", default="CCUB_eval", help="The folder path")
 args = parser.parse_args()
 
+
 folder = os.path.abspath(os.path.normpath(args.folder))
 folder_name = os.path.basename(folder)
 parent_dir = os.path.dirname(folder)
-json_file = "rename_" + folder_name + ".json"
+json_file = f"rename_{folder_name}.json"
 json_path = os.path.join(parent_dir, json_file)
 name_dict = {}
 csv_list = []
@@ -40,42 +44,48 @@ for root, dirs, files in os.walk(folder):
                 if "," in filename:
                     new_name = filename.replace(",", "")
                     name_dict[filename] = new_name
-with open(json_path, "w") as f:
-    json.dump(name_dict, f)
+                    with open(json_path, "w") as f:
+                        json.dump(name_dict, f)
 df_list = []
 for csv_path in csv_list:
-    df = pd.read_csv(csv_path)
+    # Use quotechar to handle the double quotes in the CSV files
+    df = pd.read_csv(csv_path, quotechar='"')
     df["seed"] = ""
-    # Skip the first row as it is the prompt
     for index, row in df.iterrows():
-        if index > 0:
-            seeds = []
-            for col in row.index:
-                if isinstance(row[col], str) and row[col].startswith('"') and row[col].endswith('"'):
-                    row[col] = row[col][1:-1]
-                    dir, filename = os.path.split(row[col])
-                    if filename.endswith(".png"):
-                        csv_set.add(filename)
-                        png_count += 1
-                        # Check if the filename is valid and exists in the folder
-                        if os.path.isfile(os.path.join(parent_dir, row[col])):
-                            if filename in name_dict:
-                                filename = name_dict[filename]
-                                row[col] = os.path.join(dir, filename)
-                            else:
-                                not_in_dict += 1
-                            seed = get_seed(filename)
-                            seeds.append(seed)
+        seeds = []
+        for col in row.index:
+            # Check if the column contains a PNG file
+            if ".png" in row[col]:
+                dir, filename = os.path.split(row[col])
+                # Use os.path.splitext to get the file extension
+                name, ext = os.path.splitext(filename)
+                if ext == ".png":
+                    csv_set.add(filename)
+                    png_count += 1
+                    # Check if the filename is valid and exists in the folder
+                    if os.path.isfile(os.path.join(parent_dir, row[col])):
+                        if filename in name_dict:
+                            filename = name_dict[filename]
+                            row[col] = os.path.join(dir, filename)
                         else:
-                            # Print an error message
-                            print(f"Error: {filename} is not a valid or existing file.")
+                            not_in_dict += 1
+                        seed = get_seed(filename)
+                        # Check if the seed is not None before appending it
+                        if seed is not None:
+                            seeds.append(seed)
+                    else:
+                        # Print an error message
+                        print(f"Error: {filename} is not a valid or existing file.")
                     row[col] = os.path.join(parent_dir, row[col])
-            if len(set(seeds)) == 1:
-                seed = seeds[0]
-                df.loc[index, "seed"] = seed
-            else:
-                # Print a warning with the csv file name, the row entry and the seeds
-                print(f"Warning: The seeds in row {index} of {csv_path} are not consistent. The row entry is: {row}. The seeds are: {seeds}.")
+        # Use df.drop_duplicates to remove duplicate rows
+        unique_seeds = pd.Series(seeds).drop_duplicates()
+        if len(unique_seeds) == 1:
+            seed = unique_seeds[0]
+            # Convert the seed to a string before assigning it
+            df.loc[index, "seed"] = str(seed)
+        else:
+            # Print a warning with the csv file name, the row entry and the seeds
+            print(f"Warning: The seeds in row {index} of {csv_path} are not consistent. The row entry is: {row}. The seeds are: {seeds}.")
     df_list.append(df)
 percentage = round((not_in_dict / png_count) * 100, 2) if png_count > 0 else 0
 print(f"{percentage}% of {png_count} PNG files are not in the dictionary. This means that {not_in_dict} of the PNG files in the folder have not been renamed or do not exist.")
