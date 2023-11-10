bf3f6ef12add7070d61f1bdb2e9e9c19b6df0392
Andrew Hundt <athundt@gmail.com>
2023-11-09 19:58:35 -0500

    jinja_test.html survey_template.html html generated in python

--- jinja_test.py
+++ jinja_test.py
@@ -37,21 +37,13 @@ def format_for_mturk_substitution(number_of_images):
     # Format the strings for Amazon Mechanical Turk substitution
     return [f"image{i+1}" for i in range(number_of_images)]
 
-def generate_survey_template(country_csv_file, survey_items_csv, template_html, url_prefix, output_folder="output_surveys"):
-    # Load data from CSV files
+
+
+def generate_survey_template(country_csv_file, survey_items_csv, template_html, short_instructions, full_instructions, output_folder="output_surveys"):
     country_data = pd.read_csv(country_csv_file)
     survey_items_data = pd.read_csv(survey_items_csv)
     country_name = get_country_name(country_csv_file)
-
-    column_headers = get_png_column_headers(country_data)
-    # Determine the number of images
-    number_of_images = len(column_headers)
-
-    # Update image paths
-    country_data = update_image_paths(country_data, url_prefix)
-
-    # Format strings for Amazon Mechanical Turk substitution
-    mturk_image_columns = format_for_mturk_substitution(number_of_images)
+    number_of_images = len(get_png_column_headers(country_data))
 
     # Create a Jinja2 environment
     env = Environment(loader=FileSystemLoader('.'))
@@ -59,28 +51,59 @@ def generate_survey_template(country_csv_file, survey_items_csv, template_html, 
     # Load the survey template HTML
     template = env.get_template(template_html)
 
-    survey_items = []
-    for _, row in survey_items_data.iterrows():
-        survey_item = {
-            'item_type': row['Item Type'],
-            'item_title': row['Item Title'],
-            'item_text': row['Item Text'],
-        }
-        
-        # Add image columns for Amazon Mechanical Turk substitution
+    def make_images_block(number_of_images):
+        images_block = ''
         for i in range(number_of_images):
-            survey_item[f"image{i+1}"] = f"image{i+1}"
+            images_block += f"""
 
-        survey_items.append(survey_item)
+                                <div data-id="{i}" class="image">
+                                    <span class="number">{i}</span>
+                                    <img src="image{i}" alt="Image {i}">
+                                </div>
+                            """
+        return images_block
 
+    # Prepare container block (simplified for Jinja)
+    container_block = ''
+    for _, row in survey_items_data.iterrows():
+        images_block = make_images_block(number_of_images)
+        container_block += f"""
+        <div class="sortable">
+            <div class="item">
+              <h2>{row['Item Title']}</h2>
+              <p>{row['Item Text']}</p>
+              {images_block}
+              <!-- Simplified conditions for other item types if needed -->
+            </div>
+        </div>
+        """
+
+    # Crowd-form string substitution
+    crowd_form = f"""
+        <crowd-form>
+            <div class="container">
+                {container_block}
+            </div>
+            <!-- Additional Crowd HTML Elements for Mechanical Turk -->
+            <short-instructions>
+                <p>{short_instructions}</p>
+            </short-instructions>
+            <full-instructions>
+                <p>{full_instructions}</p>
+                <!-- Additional detailed instructions go here -->
+            </full-instructions>
+        </crowd-form>
+    """
 
     # Process data and render the template
-    rendered_template = template.render(
-        country=country_name,
-        survey_items=survey_items,
-        number_of_images=number_of_images,
-        number_of_survey_items=len(survey_items_data),
-    )
+    try:
+        rendered_template = template.render(
+            country=country_name,
+            crowd_form=crowd_form
+        )
+    except Exception as e:
+        print(f"Jinja2 Error: {e}")
+        # Handle the error accordingly, e.g., raise it again, log it, or provide a default template.
 
     # Create the output folder if it doesn't exist
     if not os.path.exists(output_folder):
@@ -97,9 +120,10 @@ def generate_survey_template(country_csv_file, survey_items_csv, template_html, 
 country_csv_file = "m3c_eval/China/China_files.csv"
 survey_items_csv = "human_survey_items.csv"
 template_html = "survey_template.html"
-url_prefix = "https://raw.githubusercontent.com/ahundt/m3c_eval/main"
+short_instructions = "Rank the images or provide ratings as instructed."
+full_instructions = "Follow the instructions carefully and provide your rankings or ratings accordingly."
 
-output_file_path = generate_survey_template(country_csv_file, survey_items_csv, template_html, url_prefix)
+output_file_path = generate_survey_template(country_csv_file, survey_items_csv, template_html, short_instructions, full_instructions)
 
 # Print the path to the saved file
 print(f"Survey template saved to: {output_file_path}")
