c56510de12ef80fc1772f08dedb0493729b1bf2e
Andrew Hundt <athundt@gmail.com>
2023-11-11 23:54:28 -0500

    analyze_survey_results.py process a directory of results

--- analyze_survey_results.py
+++ analyze_survey_results.py
@@ -1,6 +1,7 @@
 import argparse
 import pandas as pd
 import json
+import os
 
 
 def extract_and_process_entry(entry, value):
@@ -33,7 +34,7 @@ def statistical_analysis(data):
     data.to_csv("statistical_output.csv")
 
 
-def process_survey_results_csv(csv_file):
+def process_survey_results_csv(csv_file, survey_items_file):
     # Load CSV Data Using Pandas
     df = pd.read_csv(csv_file)
 
@@ -43,6 +44,23 @@ def process_survey_results_csv(csv_file):
     # Concatenate the new DataFrame with the original one
     df = pd.concat([df, ratings_df], axis=1)
 
+    # Extract country name from the survey results "Title" column
+    country_name = df['Title'].str.extract(r"\((.*?)\)", expand=False)
+
+    # Get the input image columns like "Input.img1" to find the number of images per item
+    image_columns = [col for col in df.columns if col.startswith("Input.img")]
+    num_images_per_item = len(image_columns)
+
+    # Determine the number of items for analysis from human_survey_items.csv
+    human_survey_items = pd.read_csv(survey_items_file)
+    num_items = len(human_survey_items)
+
+    # You can now perform further analysis based on the extracted data
+    print(f"Country: {country_name.iloc[0]}")
+    print(f"Number of Items: {num_items}")
+    print(f"Number of Images per Item: {num_images_per_item}")
+    print(df)
+
     # Call Statistical Analysis Function
     statistical_analysis(df)
 
@@ -50,10 +68,20 @@ def process_survey_results_csv(csv_file):
 def main():
     # Command Line Parameter Parsing
     parser = argparse.ArgumentParser(description="Survey Data Analysis")
-    parser.add_argument("--csv_file", type=str, default="Batch_393773_batch_results.csv", help="Path to the input CSV file")
+    parser.add_argument("--response_results", type=str, default="Batch_393773_batch_results.csv", help="Path to the file or folder containing CSV files with Amazon Mechanical Turk survey response results.")
+    parser.add_argument("--survey_items_file", type=str, default="human_survey_items.csv", help="Path to the human_survey_items.csv file")
     args = parser.parse_args()
 
-    process_survey_results_csv(args.csv_file)
+    if os.path.isfile(args.response_results):
+        csv_files = [args.response_results]
+    elif os.path.isdir(args.response_results):
+        # List all CSV files in the results folder
+        csv_files = [os.path.join(args.results_folder, filename) for filename in os.listdir(args.response_results) if filename.endswith(".csv")]
+
+    # Loop through all CSV files and call process_survey_results_csv on each
+    for csv_file in csv_files:
+        process_survey_results_csv(csv_file, args.survey_items_file)
+
 
 if __name__ == "__main__":
     main()
