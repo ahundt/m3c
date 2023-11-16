0c383d1c772dd7b921725b651ad5cf7115e8151c
Andrew Hundt <athundt@gmail.com>
2023-11-16 02:28:10 -0500

    plot_ranking.py better visibility

--- plot_ranking.py
+++ plot_ranking.py
@@ -5,34 +5,49 @@ import argparse
 import os
 import numpy as np
 
-def scatter_y_data(data, y_column, scatter_range=0.2):
+country_markers = {
+    'China': '#DE2910',
+    'India': '#FF9933',
+    'Mexico': '#006847',
+    'Korea': '#003478',
+    'Nigeria': '#008751',
+}
+
+markers = {
+    'China': 'o',
+    'India': 's',
+    'Mexico': '^',
+    'Korea': 'D',
+    'Nigeria': 'P',
+}
+
+def scatter_y_data(data, y_column, scatter_range=0.25):
     scattered_data = data.copy()
-    scattered_data[y_column] += np.random.uniform(-scatter_range, scatter_range, size=len(scattered_data))
+    # scattered_data[y_column] += np.random.uniform(-scatter_range, scatter_range, size=len(scattered_data))
+    # adjust by linearly spacing the values with numpy
+    scattered_data[y_column] += np.linspace(-scatter_range, scatter_range, len(scattered_data))
     return scattered_data
 
 def strip_plot_rank(data, x, y, hue, filename='plot', size=(8, 6), palette=None, file_format='png', show_plot=True):
     plt.figure(figsize=size)
     
-    sns.stripplot(data=data, x=x, y=y, hue=hue, palette=palette, jitter=0.2, size=24, edgecolor='black', linewidth=1.5, marker='o')
+    sns.stripplot(data=data, x=x, y=y, hue=hue, palette=country_markers, jitter=0.25,
+                  size=24, edgecolor='black', linewidth=1.75)
     
     plt.xlabel('Neural Network Model', fontsize=14, fontweight='bold')
     plt.ylabel('Rank', fontsize=14, fontweight='bold')
     plt.title('Rankings by Neural Network Model', fontsize=18, fontweight='bold')
-    plt.legend(loc='lower left', bbox_to_anchor=(0, 0), fontsize=10)  # Move legend to bottom left inside the chart
-    plt.gca().invert_yaxis()  # Reverse the Y-axis
+    plt.legend(loc='lower left', bbox_to_anchor=(0, 0), fontsize=10)
+    plt.gca().invert_yaxis()
     plt.yticks(fontsize=12)
     plt.xticks(fontsize=12)
     plt.tight_layout()
     
-    # Adding separator lines between each neural network model
     for i in range(len(data[x].unique()) - 1):
         plt.axvline(i + 0.5, color='gray', linestyle='--', linewidth=1)
 
-    output_filename_png = f'{filename}.png'
-    plt.savefig(output_filename_png, format='png')
-    
-    output_filename_pdf = f'{filename}.pdf'
-    plt.savefig(output_filename_pdf, format='pdf')
+    output_filename_png = f'{filename}.{file_format}'
+    plt.savefig(output_filename_png, format=file_format)
     
     if show_plot:
         plt.show()
@@ -63,10 +78,9 @@ def main():
             print("Required columns are missing in the CSV file.")
             return
 
-        # Scatter the y-coordinates within ±0.2
         data = scatter_y_data(data, 'Rank', scatter_range=0.2)
 
-        file_name = os.path.splitext(input_filename)[0]  # Get filename without extension
+        file_name = os.path.splitext(input_filename)[0]
         strip_plot_rank(data, x='Neural Network Model', y='Rank', hue='Country',
                         filename=file_name, size=(8, 6), file_format=output_format, show_plot=show_plot)
     except pd.errors.EmptyDataError:
