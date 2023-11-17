18001c62dcafff57045a474f6f53f96eca44f7e1
Andrew Hundt <athundt@gmail.com>
2023-11-16 23:44:39 -0500

    analyze_survey_results.py clearer mmsr strip plot title

--- analyze_survey_results.py
+++ analyze_survey_results.py
@@ -216,7 +216,9 @@ def assess_worker_responses(
         hue = 'Country'
     if 'Item Title' in binary_rank_reconstruction_grouping_columns:
         hue = 'Item Title'
-    plot_ranking.strip_plot_rank(rank_results, x='Neural Network Model', y='Rank', hue=hue, filename=f"mmsr_rank_results-{binary_rank_reconstruction_grouping_columns_str}", show_plot=False)
+    
+    title = f"Model Rankings by {', '.join(binary_rank_reconstruction_grouping_columns)}"
+    plot_ranking.strip_plot_rank(rank_results, x='Neural Network Model', y='Rank', hue=hue, filename=f"mmsr_rank_results-{binary_rank_reconstruction_grouping_columns_str}", show_plot=False, title=title)
 
     return rank_results, results_df, worker_skills
 
